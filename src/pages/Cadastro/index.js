50c59933fd37c45d46a724f608cf5556330e3974
Adrian-Oliveira <adrianalves483@gmail.com>
2022-01-19 18:33:48 -0300

    Adição das áreas de formulário e banner na página de cadastro

--- src/pages/Cadastro/index.js
+++ src/pages/Cadastro/index.js
@@ -1,12 +1,31 @@
 import React, {Fragment} from "react";
 
+import imgCadastro from '../../core/assets/img/imagem_cadastro.png'
+import logoCadastro from '../../core/assets/icons/logo_cadastro.png'
+
 import './cadastro.scss'
 
 const Cadastro = () => {
     
     return (
         <Fragment>
-            <div className='cadastro' > Cadastro </div>
+            <div className="cadastro" >
+
+                <div className="bannerCadastro">
+                    <img className="imgCadastro" src={imgCadastro}/>
+                    <h2>
+                        Redação ilustrada
+                    </h2>
+                    <p>
+                        Bem-vindo(a), o MinhaCena.Org é uma organização sem fins lucrativos,
+                        que conecta professores de Português e Artes à Ilustradores voluntários.
+                    </p>
+                </div>
+
+                <div className="formCadastro">
+                    <img className="logoForm" src={logoCadastro} />
+                </div>
+            </div>
         </Fragment>
     );
 }
