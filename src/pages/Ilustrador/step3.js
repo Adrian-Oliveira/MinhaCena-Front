7413c5dd114f9c6df05a879cac4fd2c91246d12a
Adrian-Oliveira <adrianalves483@gmail.com>
2022-02-11 15:32:04 -0300

    Step3 cadastro ilustrador

--- src/pages/Ilustrador/step3.js
+++ src/pages/Ilustrador/step3.js
@@ -1,11 +1,29 @@
-import React,{ Fragment } from "react";
+import React,{ Fragment, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { FormCadastroActions, useCadastro } from "../../core/utils/contextCadastro";
 
 const IlustradorStep3 = ()=>{
+
+    const navigate = useNavigate();
+
+    const {state,dispatch} = useCadastro();
+
+    useEffect(()=>{
+        if(state.portfolio === '' || state.socialnetwork === '' || state.contact === ''){
+            navigate('../step2')
+        }
+    },[])
+
+    const handleClickNextStep = () =>{
+        navigate('/', {replace:true})
+    }
 
     return(
         <Fragment>
             <div>
                 Step3
+                <button onClick={handleClickNextStep}>Voltar para home</button>
             </div>
         </Fragment>
     );
