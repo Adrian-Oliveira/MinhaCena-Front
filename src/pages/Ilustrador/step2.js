4f4da27ca178b3744340ba0ffe3d78a01a1ffc16
Adrian-Oliveira <adrianalves483@gmail.com>
2022-02-11 15:27:37 -0300

    Step2 cadastro ilustrador

--- src/pages/Ilustrador/step2.js
+++ src/pages/Ilustrador/step2.js
@@ -1,11 +1,91 @@
-import React,{ Fragment } from "react";
+import React,{ Fragment, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+import {FormCadastroActions, useCadastro } from '../../core/utils/contextCadastro'
+
 
 const IlustradorStep2 = ()=>{
+
+    const navigate = useNavigate();
+
+    const {state, dispatch} = useCadastro();
+
+    useEffect(()=>{
+        if(state.name === '' || state.rg === '' || state.birthday === ''  ||  state.email === ''){
+            navigate('../step1')
+        }
+
+    }, [])
+
+    
+    const handleClickNextStep = () =>{
+        if(state.portfolio === '' || state.socialnetwork === '' || state.contact === ''){
+
+            alert(`Preencha todos os campos`);
+        }
+        else{
+            
+            navigate('../step3')
+        }
+    }
+
+    const handleChangePortfolio = (event) => {
+        dispatch({
+            type: FormCadastroActions.setPortfolio,
+            payload: event.target.value
+        });
+    };
+
+    const handleChangeSocialNetwork = (event) => {
+        dispatch({
+            type: FormCadastroActions.setSocialNetwork,
+            payload: event.target.value
+        });
+    };
+
+    const handleChangeContact = (event) => {
+        dispatch({
+            type: FormCadastroActions.setContact,
+            payload: event.target.value
+        });
+    };
 
     return(
         <Fragment>
             <div>
+                <div onClick={()=> navigate('../step1')}>
+                    Anterior
+                </div>
                 Step2
+
+                <label>
+                    Link do portfólio 
+                    <input 
+                        type='text' 
+                        value={state.portfolio}
+                        onChange={handleChangePortfolio}
+                        />
+                </label>
+
+                <label>
+                    Redes sociais 
+                    <input 
+                        type='text' 
+                        value={state.socialnetwork}
+                        onChange={handleChangeSocialNetwork}
+                        />
+                </label>
+
+                <label>
+                    Contato 
+                    <input 
+                        type='text' 
+                        value={state.contact}
+                        onChange={handleChangeContact}
+                        />
+                </label>
+
+                <button onClick={handleClickNextStep}>Cadastrar</button>
             </div>
         </Fragment>
     );
